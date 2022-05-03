e25228fd3348705f0b250ee8cacbb0fc1f810c7a
andreluismoreira <andreluistma@gmail.com>
2022-05-03 11:50:25 -0300

    adição do spring actuator

--- cadastro/src/main/java/com/cadastro/cadastro/controller/UsuarioController.java
+++ cadastro/src/main/java/com/cadastro/cadastro/controller/UsuarioController.java
@@ -5,16 +5,18 @@ import com.cadastro.cadastro.DTO.UsuarioResponse;
 import com.cadastro.cadastro.service.UsuarioService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@Api("Primeira-avaliação-web")
+//@Api("Primeira-avaliação-web")
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name = "Cadastro de usuarios", description = "Crud completo da entidade usuario")
 public class UsuarioController {
 
     @Autowired
@@ -24,25 +26,35 @@ public class UsuarioController {
         this.service = service;
     }
 
-    @PostMapping
+    @Operation(summary = "Cria um usuario", description = "cria um novo usuario na base de dados")
+    @ApiResponse( responseCode = "201", description = "criação de usuario execultada com sucesso")
+    @PostMapping(produces = {"application/json","application/xml"})
     @ApiOperation(value = "Salva um novo usuário")
     public ResponseEntity salvar(@RequestBody UsuarioRequest request){
         service.salvarUsuario(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping
+    @Operation(summary = "Lista os usuarios", description = "lista todos os usuarios na base de dados")
+    @ApiResponse( responseCode = "200", description = "listagem de usuarios execultada com sucesso")
+    @GetMapping(produces = {"application/json","application/xml"})
     @ApiOperation(value= "Lista todos os usuários do banco")
     public ResponseEntity<?> listarTodos(){
         return ResponseEntity.ok(this.service.listar());
     }
 
+    @Operation(summary = "Lista um usuario", description = "lista o usuario selecionado da base de dados")
+    @ApiResponse( responseCode = "200", description = "retorno de usuario execultada com sucesso")
+    @ApiResponse( responseCode = "404", description = "usuario não encontrado")
     @GetMapping("/{id}")
     @ApiOperation(value= "Busca um usuário no banco")
     public UsuarioResponse buscarPorId(@PathVariable Long id){
         return service.buscarPorId(id);
     }
 
+    @Operation(summary = "atualiza um usuario", description = "atualiza o usuario selecionado na base de dados")
+    @ApiResponse( responseCode = "404", description = "usuario não encontrado")
+    @ApiResponse( responseCode = "200", description = "atualização de usuario execultada com sucesso")
     @PutMapping("/{id}")
     @ApiOperation(value= "Atualiza um usuário no banco")
     public ResponseEntity<?> atualizar(@RequestBody UsuarioRequest request, @PathVariable Long id) {
@@ -50,6 +62,9 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @Operation(summary = "apaga um usuarios", description = "apaga o usuario selecionado da base de dados")
+    @ApiResponse( responseCode = "204", description = "usuario apagado com sucesso")
+    @ApiResponse( responseCode = "404", description = "usuario não encontrado")
     @DeleteMapping("/{id}")
     @ApiOperation(value= "Deleta um usuário do banco")
     public ResponseEntity deletarPorId(@PathVariable Long id) {
